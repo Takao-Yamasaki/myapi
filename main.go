600a5ab3e64ee57a3dc9438ddbc896b081bc26a5
山﨑 貴生 <yamasaki-ta@MACN0049.local>
2022-12-01 17:56:43 +0900

    ハンドラの追加

--- main.go
+++ main.go
@@ -16,8 +16,38 @@ func main() {
 		io.WriteString(w, "Hello, world!\n")
 	}
 
+	postArticleHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Article ... \n")
+	}
+
+	// ブログ一覧を取得するハンドラ
+	articleListHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Article List\n")
+	}
+
+	// 記事No.xxの投稿データを取得するエンドポイント
+	articleDetailHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Article No.1\n")
+	}
+
+	// 記事にいいね！をつけるハンドラ
+	postNiceHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Nice...\n")
+	}
+
+	// 記事にコメントを投稿するハンドラ
+	postCommentHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Comment...\n")
+	}
+
 	// 定義したハンドラをサーバーで使用するように登録
-	http.HandleFunc("/", helloHandler)
+	// パスとハンドラを対応づける
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/article", postArticleHandler)
+	http.HandleFunc("/article/list", articleListHandler)
+	http.HandleFunc("/article/1", articleDetailHandler)
+	http.HandleFunc("/article/nice", postNiceHandler)
+	http.HandleFunc("/comment", postCommentHandler)
 
 	// サーバ起動時のログを出力する
 	log.Println("server start at port 8080")
