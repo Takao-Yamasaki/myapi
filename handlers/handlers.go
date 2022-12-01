67bd6cd2cc5cecd563c41419883a423cebc19c5a
山﨑 貴生 <yamasaki-ta@MACN0049.local>
2022-12-01 18:19:33 +0900

    パッケージの分割

---
+++ handlers/handlers.go
@@ -0,0 +1,40 @@
+// ハンドラの定義を記述
+package handlers
+
+import (
+	"io"
+	"net/http"
+)
+
+// ハンドラ: HTTPリクエストを受け取って、それに対するHTTPレスポンスの内容をコネクションに書き込む
+// 引数にhttp.ResponseWriter型とhttp.Request型をとる。引数はなし。
+
+// ハンドラの定義
+func HelloHandler(w http.ResponseWriter, req *http.Request) {
+	// req: http.Requestで受けとって、w: http.ResponseWriterに書き込む
+	io.WriteString(w, "Hello, world!\n")
+}
+
+func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Article ... \n")
+}
+
+// ブログ一覧を取得するハンドラ
+func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Article List\n")
+}
+
+// 記事No.xxの投稿データを取得するエンドポイント
+func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Article No.1\n")
+}
+
+// 記事にいいね！をつけるハンドラ
+func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Nice...\n")
+}
+
+// 記事にコメントを投稿するハンドラ
+func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Comment...\n")
+}
--- main.go
+++ main.go
@@ -1,53 +1,22 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
+
+	"github.com/yourname/reponame/handlers"
 )
 
 func main() {
-	// ハンドラ: HTTPリクエストを受け取って、それに対するHTTPレスポンスの内容をコネクションに書き込む
-	// 引数にhttp.ResponseWriter型とhttp.Request型をとる。引数はなし。
-
-	// ハンドラの定義
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		// req: http.Requestで受けとって、w: http.ResponseWriterに書き込む
-		io.WriteString(w, "Hello, world!\n")
-	}
-
-	postArticleHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Posting Article ... \n")
-	}
-
-	// ブログ一覧を取得するハンドラ
-	articleListHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Article List\n")
-	}
-
-	// 記事No.xxの投稿データを取得するエンドポイント
-	articleDetailHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Article No.1\n")
-	}
-
-	// 記事にいいね！をつけるハンドラ
-	postNiceHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Posting Nice...\n")
-	}
-
-	// 記事にコメントを投稿するハンドラ
-	postCommentHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Posting Comment...\n")
-	}
-
+	
 	// 定義したハンドラをサーバーで使用するように登録
 	// パスとハンドラを対応づける
-	http.HandleFunc("/hello", helloHandler)
-	http.HandleFunc("/article", postArticleHandler)
-	http.HandleFunc("/article/list", articleListHandler)
-	http.HandleFunc("/article/1", articleDetailHandler)
-	http.HandleFunc("/article/nice", postNiceHandler)
-	http.HandleFunc("/comment", postCommentHandler)
+	http.HandleFunc("/hello", handlers.HelloHandler)
+	http.HandleFunc("/article", handlers.PostArticleHandler)
+	http.HandleFunc("/article/list", handlers.ArticleListHandler)
+	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	http.HandleFunc("/comment", handlers.PostCommentHandler)
 
 	// サーバ起動時のログを出力する
 	log.Println("server start at port 8080")
