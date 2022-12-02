68c57fe9f75b6ac1f2e4f086947ca89e5005af34
山﨑 貴生 <yamasaki-ta@MACN0049.local>
2022-12-02 15:19:16 +0900

    クエリパラメータの取得

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,8 +2,12 @@
 package handlers
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 // ハンドラ: HTTPリクエストを受け取って、それに対するHTTPレスポンスの内容をコネクションに書き込む
@@ -12,21 +16,21 @@ import (
 // ハンドラの定義
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Hello world!\n")
-	
+
 	/*
-	// req: http.Requestで受けとって、w: http.ResponseWriterに書き込む
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Hello, world!\n")
-	} else {
-		// Invalid methodを405番のステータスコードとともに返す
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-	}
+		// req: http.Requestで受けとって、w: http.ResponseWriterに書き込む
+		if req.Method == http.MethodGet {
+			io.WriteString(w, "Hello, world!\n")
+		} else {
+			// Invalid methodを405番のステータスコードとともに返す
+			http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		}
 	*/
 }
 
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Posting Article ... \n")
-	
+
 	/*if req.Method == http.MethodPost {
 		io.WriteString(w, "Posting Article ... \n")
 	} else {
@@ -37,52 +41,80 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 
 // ブログ一覧を取得するハンドラ
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article List\n")
-	
-	/*
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Article List\n")
+	// クエリパラメータを取得
+	queryMap := req.URL.Query()
+
+	var  page int
+	// パラメータのpageが一つ以上あるなら、
+	// キー:pageの存在確認 true, falseをokに格納
+	if p, ok := queryMap["page"]; ok && len(p) > 0 {
+		// パラメータpageに対応する１つ目の値を取得し、数値に変換する
+		var err error
+		page , err = strconv.Atoi(p[0])
+		// 数値に変換できない場合は400番エラーを返す
+		// 400: ユーザーリクエストの値が不正である
+		if err != nil {
+			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+			return
+		}
+	// パラメータが存在しなかった場合は1を付与する
 	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		page = 1
 	}
+	
+	resString := fmt.Sprintf("Article List (page %d)\n", page)
+	io.WriteString(w, resString)
+
+	/*
+		if req.Method == http.MethodGet {
+			io.WriteString(w, "Article List\n")
+		} else {
+			http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		}
 	*/
 }
 
 // 記事No.xxの投稿データを取得するエンドポイント
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article No.1\n")
-	
-	/*
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Article No.1\n")
-	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
+	if err != nil {
+		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+		return
 	}
+	resString := fmt.Sprintf("Article No.%d\n", articleID)
+	io.WriteString(w, resString)
+
+	/*
+		if req.Method == http.MethodGet {
+			io.WriteString(w, "Article No.1\n")
+		} else {
+			http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		}
 	*/
 }
 
 // 記事にいいね！をつけるハンドラ
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Posting Nice...\n")
-	
+
 	/*
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "Posting Nice...\n")
-	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-	}
+		if req.Method == http.MethodPost {
+			io.WriteString(w, "Posting Nice...\n")
+		} else {
+			http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		}
 	*/
 }
 
 // 記事にコメントを投稿するハンドラ
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Posting Comment...\n")
-	
+
 	/*
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "Posting Comment...\n")
-	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-	}
+		if req.Method == http.MethodPost {
+			io.WriteString(w, "Posting Comment...\n")
+		} else {
+			http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		}
 	*/
 }
