c22a838a21221f47c2555b21db9a13c0e9d96326
山﨑 貴生 <yamasaki-ta@MACN0049.local>
2022-12-21 10:28:16 +0900

    サービスインターフェースの分離

--- controllers/services/services.go
+++ controllers/services/services.go
@@ -2,11 +2,16 @@ package services
 
 import "github.com/yourname/reponame/models"
 
-type MyAppServicer interface {
+// article関連のメソッド
+type ArticleServicer interface {
 	PostArticleService(article models.Article) (models.Article, error)
 	GetArticleListService(page int) ([]models.Article, error)
 	GetArticleService(articleID int) (models.Article, error)
 	PostNiceService(article models.Article) (models.Article, error)
 
+}
+
+// comment関連のメソッド
+type CommentServicer interface {
 	PostCommentService(comment models.Comment) (models.Comment, error)
 }
