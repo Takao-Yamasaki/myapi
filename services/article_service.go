8dd1cd3a0cda61bb02d119fc5367165905f14170
山﨑 貴生 <yamasaki-ta@MACN0049.local>
2022-12-26 16:24:10 +0900

    チャネルを使った実装

--- services/article_service.go
+++ services/article_service.go
@@ -5,7 +5,6 @@ package services
 import (
 	"database/sql"
 	"errors"
-	"sync"
 
 	"github.com/yourname/reponame/apperrors"
 	"github.com/yourname/reponame/models"
@@ -37,40 +36,44 @@ func (s *MyAppService) GetArticleListService(page int) ([]models.Article, error)
 }
 
 func (s *MyAppService) GetArticleService(articleID int) (models.Article, error) {
+	// 記事の詳細を取得
+	type articleResult struct {
+		article models.Article
+		err     error
+	}
+	articleChan := make(chan articleResult)
+	defer close(articleChan)
+	
+	go func(ch chan<- articleResult) {
+		article, err := repositories.SelectArticleDetail(s.db, articleID)
+		ch <- articleResult{article: article, err: err}
+	}(articleChan)
+
+	// コメント一覧を取得
+	type commentResult struct {
+		commentList *[]models.Comment
+		err         error
+	}
+	commentChan := make(chan commentResult)
+	defer close(commentChan)
+
+	go func(ch chan<- commentResult) {
+		commentList, err := repositories.SelectCommentList(s.db, articleID)
+		ch <- commentResult{commentList: &commentList, err: err}
+	}(commentChan)
+
 	var article models.Article
 	var commentList []models.Comment
 	var articleGetErr, commentGetErr error
 
-	// 待ち合わせ機構で、内部カウンタは0
-	var wg sync.WaitGroup
-	// 内部カウンタを+2
-	wg.Add(2)
-
-	var amu sync.Mutex
-	var cmu sync.Mutex
-
-	// 記事の詳細を取得
-	go func(db *sql.DB, articleID int) {
-		// 内部カウンタを-1
-		defer wg.Done()
-		newarticle, err := repositories.SelectArticleDetail(db, articleID)
-		amu.Lock()
-		article, articleGetErr = newarticle, err
-		amu.Unlock()
-	}(s.db, articleID)
-
-	// コメント一覧を取得
-	go func(db *sql.DB, articleID int) {
-		// 内部カウンタを-1
-		defer wg.Done()
-		newcommentList, err := repositories.SelectCommentList(db, articleID)
-		cmu.Lock()
-		commentList, commentGetErr = newcommentList, err
-		cmu.Unlock()
-	}(s.db, articleID)
-
-	// カウンタが0になるまで待機
-	wg.Wait()
+	for i := 0; i < 2; i++ {
+		select {
+		case ar := <-articleChan:
+			article, articleGetErr = ar.article, ar.err
+		case cr := <-commentChan:
+			commentList, commentGetErr = *cr.commentList, cr.err
+		}
+	}
 
 	if articleGetErr != nil {
 		if errors.Is(articleGetErr, sql.ErrNoRows) {
